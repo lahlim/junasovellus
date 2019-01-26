dbe0ef9fbb1454d36d346bf17c553916a0a76991
lahlim <lassi_mustonen@hotmail.com>
2019-01-26 18:32:06 +0200

    lisätty dropdown search

--- train-master/juna.js
+++ train-master/juna.js
@@ -3,10 +3,15 @@ let asemaData = [];
 // https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=100&departed_trains=0&departing_trains=100&station=TPE
 window.onload = function(e){
     haeAsemaData();
+    asematValikkoon();
     //console.log(asemaData);
     haeData(asemaData);
     
 }
+
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
 
 function haeData(asemat){
    fetch('https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=100&departed_trains=0&departing_trains=0&station=TPE')
@@ -116,63 +121,15 @@ function haeAsemaData(){
     })
 }
 
-function vertaaAsemat(asematData){
-//Initialize with the list of symbols
 
-//Find the input search box
-let textBox = document.getElementById("textBox");
-
-//Find every item inside the dropdown
-let items = document.getElementsByClassName("dropdown-item")
-function buildDropDown(values) {
-    let contents = []
-    for (let name of values) {
-    contents.push('<input type="button" class="dropdown-item" type="button" value="' + name + '"/>')
+function asematValikkoon(){
+    let vaihtoehdot = document.getElementById("options");
+    for (let i in asemaData){
+        console.log(123);
     }
-    $('#menuItems').append(contents.join(""))
-
-    //Hide the row that shows no items were found
-    $('#empty').hide()
+    //<option value="AL">Alabama</option>
 }
 
-//Capture the event when user types into the search box
-window.addEventListener('input', function () {
-    filter(search.value.trim().toLowerCase())
-})
-
-//For every word entered by the user, check if the symbol starts with that word
-//If it does show the symbol, else hide it
-function filter(word) {
-    let length = items.length
-    let collection = []
-    let hidden = 0
-    for (let i = 0; i < length; i++) {
-    if (items[i].value.toLowerCase().startsWith(word)) {
-        $(items[i]).show()
-    }
-    else {
-        $(items[i]).hide()
-        hidden++
-    }
-    }
-
-    //If all items are hidden, show the empty view
-    if (hidden === length) {
-    $('#empty').show()
-    }
-    else {
-    $('#empty').hide()
-    }
-}
-
-//If the user clicks on any item, set the title of the button as the text of the item
-$('#menuItems').on('click', '.dropdown-item', function(){
-    $('#dropdown_coins').text($(this)[0].value)
-    $("#dropdown_coins").dropdown('toggle');
-})
-
-buildDropDown(names)
-}
 
 
 
