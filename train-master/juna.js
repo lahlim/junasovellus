d9e6085e1d8224a10c32befd0b0a9cc7c3b25239
lahlim <lassi_mustonen@hotmail.com>
2019-01-28 10:17:53 +0200

    Saapuvat ja lähtevät voi valita nappuloista. jkl avataan vakiona

--- train-master/juna.js
+++ train-master/juna.js
@@ -1,15 +1,15 @@
 "use strict";
 let asemaData = [];
-let buttonState = "ARRIVAL";
-// https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=100&departed_trains=0&departing_trains=100&station=TPE
+var buttonState = "ARRIVAL";
+var kaupunki = "JY";
+
 window.onload = function(e){ 
     haeAsemaData();
-    $('.colors input[type=radio]').on('change', function() {
-        console.log(this.value);
+    haeData(kaupunki);
+    $('.colors input[type=radio]').on('change', function() {       
         buttonState = this.value;
-        haeData(buttonState);
+        haeData(kaupunki);
     });
-    console.log(asemaData);
 }
 
 // Select 2 kirjaston haku ja dropdown
@@ -18,31 +18,34 @@ $(document).ready(function() {
 });
 
 
-function haeData(asema, buttonState){
-   fetch('https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=100&departed_trains=0&departing_trains=0&station='+asema)
+function haeData(asema){
+    let url;
+    if (buttonState == "ARRIVAL"){
+        url = 'https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=50&departed_trains=0&departing_trains=0&station=';
+    } else url = 'https://rata.digitraffic.fi/api/v1/live-trains?arrived_trains=0&arriving_trains=0&departed_trains=0&departing_trains=50&station=';
+   fetch(url+asema)
   .then((res) => res.json())
   .then((data) => {
     document.getElementById('table').innerHTML = "";
-
+    
       data.forEach(function(juna, index){
         // Poistetaan tavarajunat
         
-        if (juna.trainCategory == "Cargo" ||juna.trainCategory =="Locomotive" ){//|| juna.trainType == "VET" || juna.trainType == "T"){
-            data.splice(index,1);
+        if (juna.trainCategory == "Cargo" ||juna.trainCategory =="Locomotive" ){
+            data.splice(index, 1);
         }
         for (let i in juna.timeTableRows){
             // haetaan lähtö ja pääteasema
             juna.lahtoA = juna.timeTableRows[0].stationShortCode;
             juna.paateA = juna.timeTableRows[i].stationShortCode;
-            
-            
             // haetaan aikataulunmukainen ja arvio saapumisesta
-            if (asema === juna.timeTableRows[i].stationShortCode && juna.timeTableRows[i].type == "ARRIVAL"){
+
+            if (asema === juna.timeTableRows[i].stationShortCode && juna.timeTableRows[i].type == buttonState){
                 try{
                     juna.saapArvio = juna.timeTableRows[i].liveEstimateTime;
                     
-                } catch{    }
-                juna.saapAikaTaulu = new Date(juna.timeTableRows[i].scheduledTime);
+                } catch (e){console.log(e);}
+                juna.AikaTaulu = new Date(juna.timeTableRows[i].scheduledTime);
             }
         }
         for (let i in asemaData){
@@ -53,9 +56,9 @@ function haeData(asema, buttonState){
             if (juna.paateA == asemaData[i].stationShortCode){
                 juna.paateANimi = asemaData[i].stationName;
             }
-        }
-        
+        } 
     });
+    console.log(data);
     jarjestaSaapujat(data);
     teeTable(data);
   })
@@ -63,12 +66,16 @@ function haeData(asema, buttonState){
 
 // Luodaan taulukko aseman tietojen mukaan.
 function teeTable(data){
+    let suunta = "Saapuu";
+    if (buttonState == "DEPARTURE"){
+        suunta = "Lähtee";
+    }
     let output = `<table class="table table-striped table-hover">
     <thead>
       <th class="text-muted">Juna</th>
       <th class="text-muted">Lähtöasema</th> 
       <th class="text-muted">Pääteasema</th> 
-      <th class="text-muted">Saapuu</th>
+      <th class="text-muted">${suunta}</th>
     </thead>
     <tbody>`;
     
@@ -79,14 +86,11 @@ function teeTable(data){
                 <td>${data[i].trainType} ${data[i].trainNumber}</td>
                 <td>${data[i].lahtoANimi}</td>
                 <td>${data[i].paateANimi}</td>
-                <td>${formatoiAika(data[i].saapAikaTaulu)}</td>
+                <td>${formatoiAika(data[i].AikaTaulu)}</td>
             </tr>
             
         `;
-        } catch (e){
-            console.log("Taulukon luonnissta virhe(junia alle 10): junia "+i);
-            break;
-        }
+        } catch (e){   }
     };
     output +=`
     </tbody>
@@ -98,8 +102,8 @@ function teeTable(data){
 // järjestetään data asspumisjärjestykseen aikataulun mukaan
 function jarjestaSaapujat(data){
     data.sort(function(a, b){
-            if (a.saapAikaTaulu < b.saapAikaTaulu) return -1;
-            if (a.saapAikaTaulu > b.saapAikaTaulu) return 1;
+            if (a.AikaTaulu < b.AikaTaulu) return -1;
+            if (a.AikaTaulu > b.AikaTaulu) return 1;
             return 0;
         })
 }
@@ -120,12 +124,12 @@ function haeAsemaData(){
     .then((res) => res.json())
     .then((asemat) =>{
         for (let i in asemat){
-            // poistetaan "asema" stationName merkkijonosta
+            // poistetaan "asema" stationName merkkijonosta paitsi poikkeuksessa pasila autojuna-asema
+            if (asemat[i].stationName != "Pasila autojuna-asema"){
             asemat[i].stationName = asemat[i].stationName.replace("asema", "");
-          // Vain matkustaja trafiikki asemat
-          if (asemat[i].passengerTraffic == true){
+            }
+            asemat[i].stationName = asemat[i].stationName.replace("_", " ");
             asemaData.push(asemat[i]);
-          }
         }
     })
     .then(() =>{
@@ -139,13 +143,20 @@ function asematValikkoon(){
     for (let i in asemaData){
         let koodi = asemaData[i].stationShortCode;
         let nimi = asemaData[i].stationName;
+        // Hyväskylä valittuna default arvona
+        if (koodi == "JY") koodi = "JY' selected='selected'";
+        
+        // Vain matkustaja trafiikki asemat valikkoon
+        if (asemaData[i].passengerTraffic == true){
         $('#valitsin').append('<option value='+koodi+' >'+nimi+'</option>');
+        }
     }
     // kuunnellaan select boksin muutosta
     $('#valitsin').on('select2:select', function (e) {
-        console.log(this.value);
-        haeData(this.value, buttonState);
-      });    
+        kaupunki = this.value;
+        console.log(kaupunki);
+        haeData(this.value);
+      });
 }
 
 
